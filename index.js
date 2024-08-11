e240d47cb86eb21fc486b2a06608d21c36a96428
vallarasu143 <141846261+vallarasu143@users.noreply.github.com>
2024-08-11 09:55:29 +0530

    add hire button
    newupdate

--- index.js
+++ index.js
@@ -29,3 +29,47 @@ const timeout=setInterval(function(){
     }
     slide1.click();
 },3000);
+
+
+
+let dragbtn=document.getElementById('dragbutton');
+let offsetX , offsetY, isDraging=false;
+
+function Draging(e){
+    e.preventDefault();
+    if(e.type === 'mousedown' || e.type === 'touchstart'){
+        offsetX = e.type === 'mousedown' ? e.clientX - dragbtn.getBoundingClientRect().left:
+        e.touches[0].clientX - dragbtn.getBoundingClientRect().left;
+        offsetY = e.type === 'mousedown' ? e.clientY - dragbtn.getBoundingClientRect().top:
+        e.touches[0].clientY - dragbtn.getBoundingClientRect().top;
+        
+        isDraging=true;
+
+
+    }
+}
+
+function drag(e){
+    if(isDraging){
+        e.preventDefault()
+        const clientX = e.type === 'mousemove' ? e.clientX: e.touches[0].clientX;
+        const clientY = e.type === 'mousemove' ? e.clientY :e.touches[0].clientY;
+        //dragbtn.style.left=(clientX - offsetX)+'px';
+        dragbtn.style.top=(clientY - offsetY)+'px';
+    }
+}
+
+function enddrag(e){
+    isDraging=false;
+}
+
+//mouse events ---
+dragbtn.addEventListener('mousedown',Draging);
+document.addEventListener('mousemove',drag);
+document.addEventListener('mouseup',enddrag)
+
+//touch events ---
+
+dragbtn.addEventListener("touchstart",Draging);
+document.addEventListener("touchmove",drag);
+document.addEventListener("touchend", enddrag);
